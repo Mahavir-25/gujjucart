b1ede38013b22d450506ab134e9068e62bcd0336
Mahavir Sharma <sharmamahavir259@gmail.com>
2025-09-24 19:44:07 +0530

    signupurl

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
-from dashboard.views import IndexView
+from dashboard.views import IndexView,signup_view
 
 urlpatterns = [
    
     path('', IndexView.as_view(), name='index'),
+    path('signup', signup_view.as_view(), name='signup'),
+
 ]
