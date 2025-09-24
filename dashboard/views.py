d26c5a5cbc67410fd124731a9cffb63452c7569b
Mahavir Sharma <sharmamahavir259@gmail.com>
2025-09-24 19:40:35 +0530

    signupview added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django.contrib.auth import login
+from .forms import SignUpForm
 
 class IndexView(TemplateView):
     template_name = "dashboard/index.html"
+
+class signup_view(TemplateView):
+    template_name='signup.html'
+    form_class=SignUpForm
+    success_url= reverse_lazy('index')
+
+    def form_valid(self,form):
+        user= form.save()
+        login(self.request,user)
+        return super().form_valid(form)
